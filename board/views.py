ce9ecf07f2167a7cda688bdebdb2709208b487b5
binilog <anos13@naver.com>
2021-12-30 10:58:08 +0900

    pyweb

--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -14,13 +15,28 @@ def index(request):
 def boardlist(request):
     #질문 목록
     #question_list = Question.objects.all()  #db 전체조회
-    question_list = Question.objects.order_by('-create_date')#작성일 내림차순
+    # question_list = Question.objects.order_by('-create_date')#작성일 내림차순
 
     #페이지 처리
     page = request.GET.get('page', 1)  #127.0.0.1:8000/ 기본 1페이지임
+    kw = request.GET.get('kw', '')
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__username__icontains=kw) |
+            Q(answer__author__icontains=kw) |
+            Q(answer__content__icontains=kw)
+        ).distinct() #유일한것 검색
+
     paginator = Paginator(question_list, 10) #페이지당 10개씩 설정
     page_obj = paginator.get_page(page)    #페이지 가져오기
-    return render(request, 'board/question_list.html',{'question_list':page_obj})
+    context = {'question_list':page_obj, 'page':page, 'kw':kw}
+    return render(request, 'board/question_list.html', context)
 
 def detail(request, question_id):
     # 질문/답변 상세
